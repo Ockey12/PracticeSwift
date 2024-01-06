42f8054bc5b7f133255edd94613f3de523fa5d32
OnagaHaruki <haloupgarou@gmail.com>
2024-01-06 21:54:26 +0900

    Create Project

---
+++ PracticeSwift/PracticeSwift/PracticeSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeSwiftApp.swift
+//  PracticeSwift
+//
+//  Created by オナガ・ハルキ on 2024/01/06.
+//
+
+import SwiftUI
+
+@main
+struct PracticeSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
